e8c5bdc57da87bb1f46452263ec57423220ddd25
Mike Dyer <mike.dyer@carbonundone.com>
2023-04-11 10:51:21 -0600

    remove some trpc routes

--- packages/api/src/router/index.ts
+++ packages/api/src/router/index.ts
@@ -1,7 +1,9 @@
 import { router } from "../trpc";
 import { userRouter } from "./routes/user";
 
-export const appRouter = router({ user: userRouter });
+export const appRouter = router({
+  user: userRouter,
+});
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
