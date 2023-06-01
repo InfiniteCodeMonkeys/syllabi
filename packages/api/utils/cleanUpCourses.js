0241908e15c90213695ee9ba026a184f308c480d
Mike Dyer <mike.dyer@carbonundone.com>
2023-05-31 21:07:35 -0600

    courses created and formatted

--- packages/api/utils/cleanUpCourses.js
+++ packages/api/utils/cleanUpCourses.js
@@ -1,7 +1,10 @@
 const { createId } = require("@paralleldrive/cuid2");
 const fs = require("fs");
+const { get } = require("http");
 const coursesDir = "../data/courses";
 const coursesFile = "../data/courses.json";
+const subjectsFile = "../data/subjectsAndDisciplines.json";
+const uniqueCoursesFile = "../data/uniqueCourses.json";
 
 const formatSubjects = async () => {
   const files = await fs.promises.readdir(coursesDir);
@@ -33,8 +36,74 @@ const formatSubjects = async () => {
   console.log("COMPLETED");
 };
 
-formatSubjects();
+// formatSubjects();
 
 // step two remove duplicates
+const findDuplicates = () => {
+  const courses = JSON.parse(fs.readFileSync(coursesFile, "utf8"));
+  const duplicates = [];
+  const unique = [];
+  const uniqueCourses = [];
+  for (const course of courses) {
+    if (unique.includes(course.data.name)) {
+      duplicates.push(course);
+    } else {
+      unique.push(course.data.name);
+      uniqueCourses.push(course);
+    }
+  }
+  console.log("duplicates", duplicates.length);
+  console.log("unique", unique.length);
+  fs.writeFileSync(uniqueCoursesFile, JSON.stringify(uniqueCourses));
+};
+
+// findDuplicates();
 
 // step three format pre-reqs
+
+const addIdstoPreReqs = () => {
+  const courses = JSON.parse(fs.readFileSync(uniqueCoursesFile, "utf8"));
+  for (const course of courses) {
+    if (course.prerequisites) {
+      const newPreReqs = [];
+      for (let preReq of course.prerequisites) {
+        if (course.prerequisites.length && preReq) {
+          const found = courses.find((c) => c.data.name === preReq);
+          if (found) {
+            preReq = {};
+            preReq.name = found.data.name;
+            preReq.id = found.id;
+            newPreReqs.push(preReq);
+          } else {
+            console.log("not found", preReq);
+          }
+        }
+      }
+      course.prerequisites = newPreReqs;
+    }
+  }
+  fs.writeFileSync(uniqueCoursesFile, JSON.stringify(courses));
+  console.log("COMPLETED");
+};
+
+// addIdstoPreReqs();
+
+const getParentId = () => {
+  const courses = JSON.parse(fs.readFileSync(uniqueCoursesFile, "utf8"));
+  const subjects = JSON.parse(fs.readFileSync(subjectsFile, "utf8"));
+
+  for (const course of courses) {
+    if (course.parent) {
+      const found = subjects.find((c) => c.data.name === course.parent.name);
+      if (found) {
+        course.parent.id = found.id;
+      } else {
+        console.log("not found", course.parent.name);
+      }
+    }
+  }
+  fs.writeFileSync(uniqueCoursesFile, JSON.stringify(courses));
+  console.log("COMPLETED");
+};
+
+getParentId();
